5406f87f5940c4cc42c1b0dc5fabc5528b383ad4
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-08-02 20:49:00 +0300

    up

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,11 @@
-export function dateBuilder (dayNow) {
+export function dayRender(dayNow) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
+
     let day = days[dayNow.getDay()];
     let date = dayNow.getDate();
     let month = months[dayNow.getMonth()];
     let year = dayNow.getFullYear();
-  
+
     return `${day} ${date} ${month} ${year}`;
-  }
+}
--- src/requestApi.js
+++ src/requestApi.js
@@ -1,40 +1,38 @@
-import {renderResults} from './renderResult.js'
-import {renderHeder} from './renderHeder.js'
+import { renderResults } from './renderResult.js'
+import { renderHeder } from './renderHeder.js'
 
 renderHeder()
 
 const api = {
     key: "f086dcbf4b80b103f289fb59463dceeb",
     url: "https://api.openweathermap.org/data/2.5/"
-  }
-  
-  const searchBtn = document.getElementById('btn');
-  const searchInput = document.getElementById('search-box');
-  
-  searchBtn.addEventListener('click', request);
-  
-  export function request(e) {
+}
+
+const searchBtn = document.getElementById('btn');
+const searchInput = document.getElementById('search-box');
+
+searchBtn.addEventListener('click', request);
+
+export function request(e) {
     let cityName = searchInput.value;
-    if (cityName!='' && cityName.length >2){
-      e.preventDefault();
-      getResults(cityName);
-  }
-    
-  }
-  
-  function getResults (cityName) {
-  let stat = '';
-  let p = document.createElement('p')
-  
-  
-  let ulArea = document.getElementById('statusArea')
+    if (cityName != '' && cityName.length > 2) {
+        e.preventDefault();
+        getResults(cityName);
+    }
+}
+
+function getResults(cityName) {
+
+
+    let p = document.createElement('p')
+    let ulArea = document.getElementById('statusArea')
     fetch(`${api.url}weather?q=${cityName}&units=metric&APPID=${api.key}`)
-    .then(res => res.ok ? res.json()   : stat = `${res.status}`)
-    .then(document.getElementById('statusArea').innerHTML = ``)
-    .then(renderResults)
-      .catch(function(err) {
-        p.textContent = `City Not Found`
-        ulArea.appendChild(p);
-    });
-    
-  }
+        .then(res => res.ok ? res.json() : stat = `${res.status}`)
+        .then(document.getElementById('statusArea').innerHTML = ``)
+        .then(renderResults)
+        .catch(function(err) {
+            p.textContent = `City Not Found`
+            ulArea.appendChild(p);
+        });
+
+}
